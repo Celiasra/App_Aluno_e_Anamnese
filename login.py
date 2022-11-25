e513618a3ca5ef162ebb9777c6bb38938127f276
Celia Andrade <celiasra@uol.com.br>
2022-11-25 18:20:55 -0300

    subindo a primeira versão do app

---
+++ login.py
@@ -0,0 +1,75 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+#Window.clearcolor = (14/255,61/255,76/255,1)
+Window.size = (400,600)
+
+
+class Lgn(BoxLayout):
+    def logar(self):
+        usuario = self.ids.usuario.text
+        senha = self.ids.senha.text
+
+        if usuario == 'admin' and senha == 'master':
+            janela.root_window.remove_widget(janela.root)
+            janela.root_window.add_widget(Home())
+
+        else:
+            self.ids.msg.text = 'Usuário ou senha invalida'
+
+    # def on_press_bt(self):
+    #    janela.root_window.remove_widget(janela.root)
+    #    janela.root_window.add_widget(Home())
+
+
+class Home(BoxLayout):
+    def home(self):
+        janela.root_window.remove_widget(janela.root)
+        janela.root_window.add_widget(Home())
+
+    def consultar_aluno(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Consultar_Aluno())
+
+    def consultar_anamnese(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Consultar_Anamnese())
+
+    def cadastrar_anamnese(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Cadastrar_Anamnese())
+
+
+class Consultar_Aluno(BoxLayout):
+   def consultar_aluno(self):
+       pass
+
+   def voltar(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Home())
+
+
+class Consultar_Anamnese(BoxLayout):
+    def consultar_anamnese(self):
+        pass
+
+    def voltar(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Home())
+
+
+class Cadastrar_Anamnese(BoxLayout):
+    def cadastrar_anamnese(self):
+        pass
+
+    def voltar(self):
+       janela.root_window.remove_widget(janela.root)
+       janela.root_window.add_widget(Home())
+
+class Login(App):
+    def build(self):
+        return Lgn()
+
+janela = Login()
+janela.run()
